8e66d13b85d7e8dfc54e2f2daa86c83b78cb7176
ydianatpey <ydianatpey@gmail.com>
2023-09-15 21:50:48 -0700

    useState

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Container, Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
-import CampsitesDirectoryPage from '../pages/CampsitesDirectoryPage';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import Header from './app/components/Header';
 import Footer from './app/components/Footer';
 
